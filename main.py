b99f0ef4074a1aff79c8c8dcbeeaac5ca068142d
croftc94 <croft.c@hotmail.com>
2024-09-12 18:05:57 -0500

    added updatable and drawable groups to main, implmented groups

--- main.py
+++ main.py
@@ -4,19 +4,31 @@ from player import *
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+            
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+    
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
