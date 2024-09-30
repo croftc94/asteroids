9cfe78186ff77a6c4df5ed44702809c40ff916e2
croftc94 <croft.c@hotmail.com>
2024-09-30 17:49:28 -0500

    added split() method to Asteroid class, implemented to main

--- asteroid.py
+++ asteroid.py
@@ -13,3 +13,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        split_velocity_1 = self.velocity.rotate(random_angle)
+        split_velocity_2 = self.velocity.rotate(random_angle * -1)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid_1.velocity = split_velocity_1 * 1.2
+        asteroid_2.velocity = split_velocity_2 * 1.2
+
+
